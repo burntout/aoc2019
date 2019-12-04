8f666c5b362ccd021288dcff1146d4ec223ed2db
Kate Dawson <kate@webarchitects.coop>
2019-12-04 20:10:04 +0000

    fixed mistake

--- 4/4b.hs
+++ 4/4b.hs
@@ -16,7 +16,7 @@ hasRepeat (x:y:xs) = x == y || hasRepeat (y:xs)
 hasDoubleButNotTriple [a,b,c,d,e,f] = or [t1,t2,t3,t4,t5]
     where
     t1 = a == b && b /= c
-    t2 = b == c && a /= b && b /= d
+    t2 = b == c && a /= b && c /= d
     t3 = c == d && b /= c && d /= e
     t4 = d == e && c /= d && e /= f
     t5 = e == f && d /= e          
